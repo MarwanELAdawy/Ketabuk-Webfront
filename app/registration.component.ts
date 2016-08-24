5b2079982844d30db242bbdecf4426be562cec63
Adham Zahran <adham.zahran@hotmail.com>
2016-08-24 19:45:09 +0200

    on registration when user types the nickname the journalname gets created automatically with a prefix 'كراسة '

--- app/registration.component.ts
+++ app/registration.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { User } from './user';
 
 @Component(
     {
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
     }
 )
 export class RegistrationComponent
-{}
+{
+    @Input() user : User;
+    @Input() journalModified: boolean;
+    @Input() journalName: string;
+
+    constructor(){
+        this.user = new User;
+        this.journalModified = false;
+    }
+
+    setJournalModified($event)
+    {
+        this.journalModified = true;
+    }
+
+    setJournalName($event)
+    {
+        if (!this.journalModified)
+            this.journalName = "كراسة " + this.user.name;
+    }
+}

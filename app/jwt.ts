ee6774cd09c95ec8e8e6d357f66fea9c6bb67d5a
Adham Zahran <adham.zahran@hotmail.com>
2016-08-25 18:09:43 +0200

    reading headers

---
+++ app/jwt.ts
@@ -0,0 +1,6 @@
+export class JWT
+{
+    public static _JWT: string;
+    public static get JWT(): string { return this._JWT; }
+    public static set JWT(x: string) { this._JWT = x; }
+}
--- app/login.component.ts
+++ app/login.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { LoginService } from './login.service';
+import { Injectable }     from '@angular/core';
+import { Http, Response } from '@angular/http';
 
 @Component(
     {
         selector: 'my-login',
-        templateUrl: 'app/login.component.html'
+        templateUrl: 'app/login.component.html',
+        providers: [LoginService]
     }
 )
 export class LoginComponent
 {
-    
+    @Input() email: string;
+    @Input() password: string;
+
+    response: string;
+    errorMessage: any;
+
+    constructor(private loginService: LoginService, private http: Http) {}
+    login()
+    {
+        this.loginService.login([this.email, this.password])
+                            .subscribe(
+                          response => this.response = response,
+                          error =>  this.errorMessage = <any>error);
+    }
 }

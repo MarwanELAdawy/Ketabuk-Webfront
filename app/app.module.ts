c2cecfb5dacc2f8db90945611e460eb706b42d01
Adham Zahran <adham.zahran@hotmail.com>
2016-09-07 01:12:09 +0200

    typo fixes

--- app/app.module.ts
+++ app/app.module.ts
@@ -40,16 +40,3 @@ if(Config.IN_PRODUCTION)
     bootstrap:    [AppComponent],
 })
 export class AppModule {}
-
-// import { bootstrap }    from '@angular/platform-browser-dynamic';
-// import { HTTP_PROVIDERS } from '@angular/http';
-
-// import { AppComponent } from './app.component';
-// import { appRouterProviders } from './app.routes';
-// import { SuperAuth } from './super-auth';
-
-// bootstrap(AppComponent, [
-//   appRouterProviders,
-//   HTTP_PROVIDERS,
-//   SuperAuth
-// ]);

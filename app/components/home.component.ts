d4764b5b54ece354061cf05d3fde5361794211d1
Adham Zahran <adham.zahran@hotmail.com>
2017-01-26 01:53:00 +0200

    fix #78 implemented milestones progress bar using github api

--- app/components/home.component.ts
+++ app/components/home.component.ts
@@ -2,26 +2,31 @@ import { Component, OnInit } from '@angular/core';
 //import { RuntimeCompiler} from '@angular/compiler';
 
 import { JournalService } from '../services/journal.service';
+import { GithubService } from '../services/github.service';
 import { Journal } from '../models/journal';
 import { SuperAuth } from '../supers/super-auth';
 
 @Component({
     selector: 'home',
     templateUrl: 'app/templates/home.component.html',
-    providers: [JournalService]
+    providers: [JournalService, GithubService]
 })
 export class HomeComponent implements OnInit
 {
     journals: Journal[];
+    open_issues: number;
+    closed_issues: number;
+    issues_ratio: string;
     errorMessage: string;
     isLoggedIn: boolean;
-    constructor(private journalService: JournalService){}//, private _runtimeCompiler: RuntimeCompiler) {}
+    constructor(private journalService: JournalService, private githubService: GithubService){}//, private _runtimeCompiler: RuntimeCompiler) {}
     
 
     ngOnInit()
     {
         if (this.isLoggedIn = SuperAuth.isLoggedIn())
             this.getJournals();
+        else this.getMilestoneProgress();
     }
 
     getJournals()
@@ -30,5 +35,21 @@ export class HomeComponent implements OnInit
                           .subscribe(
                           journals => this.journals = journals,
                           error =>  this.errorMessage = <any>error);
+    }
+
+    getMilestoneProgress()
+    {
+        this.githubService.getMilestone()
+                          .subscribe(
+                              response => this._MilestoneProgressCallback(response),
+                              error =>  this.errorMessage = <any>error);
+    }
+
+    _MilestoneProgressCallback(response)
+    {
+        this.open_issues = response.open_issues;
+        this.closed_issues = response.closed_issues;
+        var tmp =  100 * (this.closed_issues / (this.open_issues + this.closed_issues));
+        this.issues_ratio = tmp.toFixed().toString() + "%";
     }
 }
---
+++ app/services/github.service.ts
@@ -0,0 +1,40 @@
+import { Injectable }     from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Journal }           from '../models/journal';
+import { Observable }     from 'rxjs/Observable';
+import { Config } from '../config';
+import { SuperAuth } from '../supers/super-auth';
+import { SuperService } from '../supers/super-service';
+
+@Injectable()
+export class GithubService
+{
+  constructor (private http: SuperAuth)
+  {}
+
+  private githubUrl = 'https://api.github.com/repos/Ketabuk/Ketabuk-Webfront/milestones/1';  // URL to github api
+
+  getMilestone (): Observable<any>
+  {
+      return this.http.get(this.githubUrl)
+                 .map(this.extractData)
+                 .catch(this.handleError);
+  }
+
+  private extractData(response: Response)
+  {
+    let body = response.json();
+    return body || { };
+  }
+
+  private handleError (error: any)
+  {
+    // In a real world app, we might use a remote logging infrastructure
+    // We'd also dig deeper into the error to get a better message
+    let errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(errMsg); // log to console instead
+    return Observable.throw(errMsg);
+  }
+
+}

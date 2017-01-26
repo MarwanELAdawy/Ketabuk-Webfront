87334f2b56cfe758fe04a690311b1671217ea906
Adham Zahran <adham.zahran@hotmail.com>
2017-01-26 18:44:09 +0200

    fix #82 created a notifications serivice

--- app/services/notification.service.ts
+++ app/services/notification.service.ts
@@ -6,39 +6,29 @@ import { SuperAuth } from '../supers/super-auth';
 import { SuperService } from '../supers/super-service';
 
 @Injectable()
-export class GithubService
+export class NotificationService
 {
 	constructor (private http: SuperAuth)
 	{}
 	
-	private githubWebfrontUrl = 'https://api.github.com/repos/Ketabuk/Ketabuk-Webfront/milestones/1';  // URL to github api
-	private githubServerUrl = 'https://api.github.com/repos/Ketabuk/Ketabuk-Server/milestones/1';  // URL to github api
-
-
+	private notificationsUrl = Config.API_URL + 'notifications';  // URL to web API
+	
 	getWebfrontMilestone (): Observable<any>
 	{
-			return this.http.get(this.githubWebfrontUrl)
-								 .map(this.extractData)
-								 .catch(this.handleError);
-	}
-
-	getServerMilestone (): Observable<any>
-	{
-			return this.http.get(this.githubServerUrl)
+			return this.http.get(this.notificationsUrl)
 								 .map(this.extractData)
 								 .catch(this.handleError);
 	}
 
 	private extractData(response: Response)
 	{
+		SuperService.extractData(response);
 		let body = response.json();
 		return body || { };
 	}
 
 	private handleError (error: any)
 	{
-		// In a real world app, we might use a remote logging infrastructure
-		// We'd also dig deeper into the error to get a better message
 		let errMsg = (error.message) ? error.message :
 			error.status ? `${error.status} - ${error.statusText}` : 'Server error';
 		console.error(errMsg); // log to console instead
